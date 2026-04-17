0c24ca5906b7b4f0fde4cdf252fc6f1b0b39ac2a
bwww123-hue <bwww123@gmail.com>
2026-04-17 22:50:34 +0900

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: './',   // GitHub Pages 경로 문제 해결 (상대 경로)
-})
